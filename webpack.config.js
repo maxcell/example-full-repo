3dcf2be1de52503f64825ede3a1872eaf5303ae6
Prince Wilson <maxcell.wilson@gmail.com>
2021-07-30 07:48:11 -0400

    Update the webpack config to allow for different environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path")
 
-module.exports = {
+module.exports = {  
+  mode: process.env.NODE_ENV || "development",
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
