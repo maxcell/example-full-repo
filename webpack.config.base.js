7f5a4b6992561d3cf591f8ace6b7cab540c97c7b
Prince Wilson <maxcell.wilson@gmail.com>
2021-08-01 08:10:47 -0400

    Adds separate webpack configs for different modes

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,7 +2,6 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {  
-  mode: process.env.NODE_ENV || "development",
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
