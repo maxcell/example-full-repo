6df6f99269ee9f5d2213aa6b0927f56d33ac5b29
Prince Wilson <maxcell.wilson@gmail.com>
2021-08-01 12:12:31 -0400

    Adds bundle analyzer for production builds

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,7 @@
 const {merge} = require("webpack-merge")
 const baseConfig = require('./webpack.config.base')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const {BundleAnalyzerPlugin} = require("webpack-bundle-analyzer")
 
 module.exports = merge(baseConfig, {
   mode: 'production',
@@ -12,5 +13,10 @@ module.exports = merge(baseConfig, {
         use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
-  }
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static'
+    })
+  ]
 })
